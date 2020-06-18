b54b5b51c58c5f10e4b9a88a4acc91c9089f5df9
William Woodruff <william@yossarian.net>
2020-06-18 14:13:17 -0400

    test: Add nested signature tests

--- test/helpers.h
+++ test/helpers.h
@@ -47,3 +47,35 @@ class Auth32PlusTest : public ::testing::Test {
 
   peparse::parsed_pe *pe{nullptr};
 };
+
+class AuthNest32Test : public ::testing::Test {
+ protected:
+  void SetUp() override {
+    auto *file = UTHENTICODE_TEST_ASSETS "/32/pegoat-authenticode-nested.exe";
+
+    pe = peparse::ParsePEFromFile(file);
+    ASSERT_TRUE(pe != nullptr);
+  }
+
+  void TearDown() override {
+    peparse::DestructParsedPE(pe);
+  }
+
+  peparse::parsed_pe *pe{nullptr};
+};
+
+class AuthNest32PlusTest : public ::testing::Test {
+ protected:
+  void SetUp() override {
+    auto *file = UTHENTICODE_TEST_ASSETS "/64/pegoat-authenticode-nested.exe";
+
+    pe = peparse::ParsePEFromFile(file);
+    ASSERT_TRUE(pe != nullptr);
+  }
+
+  void TearDown() override {
+    peparse::DestructParsedPE(pe);
+  }
+
+  peparse::parsed_pe *pe{nullptr};
+};
